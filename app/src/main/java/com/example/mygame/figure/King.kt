ddac183aade1413a438aed1981b1973258206f69
AntonDubrovin <dubrovinantonr@mail.ru>
2021-01-09 06:33:08 +0300

    added 0-0 and 0-0-0 + new apk

--- app/src/main/java/com/example/mygame/figure/King.kt
+++ app/src/main/java/com/example/mygame/figure/King.kt
@@ -2,6 +2,7 @@ package com.example.mygame.figure
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import com.example.mygame.Board
 import com.example.mygame.FigureColor
 import com.example.mygame.R
@@ -35,9 +36,36 @@ class King(_x: Int, _y: Int, _color: FigureColor) : AbstractFigure() {
                 }
             }
         }
+        if (color == FigureColor.WHITE && y == 7 && x == 4) {
+            if (Board.gameBoard[7][5] is Empty && Board.gameBoard[7][6] is Empty &&
+                Board.gameBoard[7][7] is Rook && Board.gameBoard[7][7].color == FigureColor.WHITE
+            ) {
+                show(canvas, width, context, 7, 6)
+            }
+            if (Board.gameBoard[7][3] is Empty && Board.gameBoard[7][2] is Empty && Board.gameBoard[7][1] is Empty &&
+                Board.gameBoard[7][0] is Rook && Board.gameBoard[7][0].color == FigureColor.WHITE
+            ) {
+                show(canvas, width, context, 7, 2)
+            }
+        } else if (color == FigureColor.BLACK && y == 0 && x == 4) {
+            if (Board.gameBoard[0][5] is Empty && Board.gameBoard[0][6] is Empty &&
+                Board.gameBoard[0][7] is Rook && Board.gameBoard[0][7].color == FigureColor.BLACK
+            ) {
+                show(canvas, width, context, 0, 6)
+            }
+            if (Board.gameBoard[0][3] is Empty && Board.gameBoard[0][2] is Empty && Board.gameBoard[0][1] is Empty &&
+                Board.gameBoard[0][0] is Rook && Board.gameBoard[0][0].color == FigureColor.BLACK
+            ) {
+                show(canvas, width, context, 0, 2)
+            }
+        }
     }
 
-    override fun makeMove(newX: Int, newY: Int, turn: FigureColor): Boolean { //TODO ДОХУЯ ВСЕГО ПОМОГИТЕ
+    override fun makeMove(
+        newX: Int,
+        newY: Int,
+        turn: FigureColor
+    ): Boolean { //TODO ДОХУЯ ВСЕГО ПОМОГИТЕ
         if (turn == color) {
             if (Board.gameBoard[newY][newX] is Empty || Board.gameBoard[newY][newX] !is Empty && Board.gameBoard[newY][newX].color != color) {
                 for (dx in -1..1) {
@@ -49,6 +77,55 @@ class King(_x: Int, _y: Int, _color: FigureColor) : AbstractFigure() {
                                 return true
                             }
                         }
+                    }
+                }
+            }
+            if (color == FigureColor.WHITE && y == 7 && x == 4) {
+                if (newX == 6 && newY == 7) {
+                    if (Board.gameBoard[7][5] is Empty && Board.gameBoard[7][6] is Empty &&
+                        Board.gameBoard[7][7].color == FigureColor.WHITE
+                    ) {
+                        x = newX
+                        y = newY
+                        Board.gameBoard[7][7].x = 5
+                        Board.gameBoard[7][5] = Board.gameBoard[7][7]
+                        Board.gameBoard[7][7] = Empty(7, 7, FigureColor.EMPTY)
+                        return true
+                    }
+                } else if (newX == 2 && newY == 7) {
+                    if (Board.gameBoard[7][3] is Empty && Board.gameBoard[7][2] is Empty && Board.gameBoard[7][1] is Empty &&
+                        Board.gameBoard[7][0].color == FigureColor.WHITE
+                    ) {
+                        x = newX
+                        y = newY
+                        Board.gameBoard[7][0].x = 3
+                        Board.gameBoard[7][3] = Board.gameBoard[7][0]
+                        Board.gameBoard[7][0] = Empty(0, 7, FigureColor.EMPTY)
+                        return true
+                    }
+                }
+            } else if (color == FigureColor.BLACK && y == 0 && x == 4) {
+                if (newX == 6 && newY == 0) {
+                    if (Board.gameBoard[0][5] is Empty && Board.gameBoard[0][6] is Empty &&
+                        Board.gameBoard[0][7].color == FigureColor.BLACK
+                    ) {
+                        x = newX
+                        y = newY
+                        Board.gameBoard[0][7].x = 5
+                        Board.gameBoard[0][5] = Board.gameBoard[0][7]
+                        Board.gameBoard[0][7] = Empty(7, 0, FigureColor.EMPTY)
+                        return true
+                    }
+                } else if (newX == 2 && newY == 0) {
+                    if (Board.gameBoard[0][3] is Empty && Board.gameBoard[0][2] is Empty && Board.gameBoard[0][1] is Empty &&
+                        Board.gameBoard[0][0].color == FigureColor.BLACK
+                    ) {
+                        x = newX
+                        y = newY
+                        Board.gameBoard[0][0].x = 3
+                        Board.gameBoard[0][3] = Board.gameBoard[0][0]
+                        Board.gameBoard[0][0] = Empty(0, 0, FigureColor.EMPTY)
+                        return true
                     }
                 }
             }
